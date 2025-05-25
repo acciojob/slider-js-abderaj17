d492dc1d353cbb707c0fc1849172e2d46e51d71e
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-05-25 21:30:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let currentSlide = 0;
+const slides = document.querySelectorAll('.left-slide');
+const totalSlides = slides.length;
+
+document.querySelector('.up-button').addEventListener('click', ()=>{
+	slides.[currentSlide].style.display = 'none';
+	currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+	slides[currentSlide].style.display = 'block';
+});
+document.querySelector('.down-button').addEventListener('click', ()=>{
+	slides[currentSlide].style.display = 'none';
+	currentSlide = (currentSlide + 1) % totalSlides;
+	slides[currentSlide].style.display = 'block';
+})
